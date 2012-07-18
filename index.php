7d36699dc4b288b448a68f0554667cc27348634a
Stephen McNabb <smcnabb@ign.com>
2012-07-17 22:26:38 -0700

    Added index page

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php include 'html/header.php'; ?>
+
+<h2>Code Foo PHP Workshop - Let's Make An Old School RPG</h2>
+
+<ol>
+    <li><a href="step1.php">Variables / Strings / Output</a></li>
+    <li><a href="step2.php">Math / Dates / Equality checks / For Loop</a></li>
+    <li><a href="step3.php">Arrays / Foreach / Switch</a></li>
+    <li><a href="step4.php">Functions / Classes / Default arguments</a></li>
+    <li><a href="step5.php">Includes/Requires</a></li>
+    <li><a href="step6.php">Embeddeding PHP in HTML / Template friendly statements</a></li>
+    <li><a href="step7.php">Cookies</a></li>
+    <li><a href="step8.php">Query strings / Forms</a></li>
+    <li><a href="step9.php">Database</a></li>
+    <li><a href="step10.php">Putting it all together</a></li>
+</ol>
+
+<? include 'html/footer.php';
